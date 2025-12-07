f2d4ba3f4058f58b6e1806402b5e4ea8edc60a2b
ARS <abhayrshanbhag@hotmail.com>
2025-12-07 21:01:24 +0530

    fix: add root entry point and update dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+// AquaChain Root Package Entry
+// This file satisfies the "main" field in package.json and allows the backend to require the root package.
+
+module.exports = {
+    name: "aquachain",
+    version: "1.0.0",
+    description: "Smart Irrigation System Root"
+};
